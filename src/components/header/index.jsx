bd1be149a780059039d24baec8964d61aa40ab8a
niltonGold <niltonenrique3000@hotmail.com>
2024-06-20 14:01:14 +0200

    header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,8 +4,12 @@ import './style.css';
 export const Header = () => {
     return (
       <header>
+        
+        {/* Contenedor de el Logo de Lullaby y los 3 circulos con enlaces (Escuela de Padres, Zona Privada, Contacto) */}
         <div className='header-enlaces-container'>
             <div className='lullaby_logo' />
+
+            {/* Contenedor de los 3 circulos del header */}
             <div className='header-enlaces'>
                 <div className='header-escuelaDePadres logoRedondoHeader' />
                 <div className='header-zonaPrivada logoRedondoHeader' />
@@ -13,40 +17,42 @@ export const Header = () => {
             </div>
         </div>
         
+        {/* Contenedor de los enlaces de navegacion (Inicio, Quiénes Somos, Instalacines, Inglés, Equipo) */}
         <div className='header-paginas-navegacion'>
-
-                
-            <div className='header-navegacion-link-container'>
-                <div className='header-navegacion-incio-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Inicio</div>
-            </div>
-          
-            <div className='header-navegacion-link-container'>
-                <div className='header-navegacion-quienesSomos-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Quiénes Somos</div>
-            </div>
-          
-            <div className='header-navegacion-link-container'>
-                <div className='header-navegacion-instalaciones-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Instalaciones</div>
-            </div>
-
-            <div className='header-navegacion-link-container'>
-                <div className='header-navegacion-proyectoLullaby-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Proyectos</div>
-            </div>
-
-            <div className='header-navegacion-link-container'>
-                <div className='header-navegacion-ingles-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Inglés</div>
-            </div>
-
-            <div className='header-navegacion-link-container header-navegacion-link-container-final '>
-                <div className='header-navegacion-equipo-logo logoNavegacion' />
-                <div className='header-navegacion-texto'>Equipo</div>
-            </div> 
+   
+            <div className='header-paginas-navegacion-subcontainer'>
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo' />
+                    <div className='header-navegacion-texto'>Inicio</div>
+                </div>
             
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo header-logo-hide' />          
+                    <div className='header-navegacion-texto'>Quiénes Somos</div>
+                </div>
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo header-logo-hide' /> 
+                    <div className='header-navegacion-texto'>Instalaciones</div>        
+                </div>
+
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo header-logo-hide' />
+                    <div className='header-navegacion-texto'>Proyectos</div>        
+                </div>
+                
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo header-logo-hide' /> 
+                    <div className='header-navegacion-texto'>Inglés</div>
+                </div>
+                            
+                <div className='header-navegacion-link-container'>
+                    <div className='header-navegacion-incio-logo header-logo-hide' /> 
+                    <div className='header-navegacion-texto'>Equipo</div>
+                </div>
+            </div>
+                
         </div>
+            
       </header>
 
   )
