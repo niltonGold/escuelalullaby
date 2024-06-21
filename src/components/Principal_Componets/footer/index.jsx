ff1b8de467942f10d50ad5fdc279be59e2bb843f
niltonGold <niltonenrique3000@hotmail.com>
2024-06-21 11:21:41 +0200

    inicio

--- src/components/Principal_Componets/footer/index.jsx
+++ src/components/Principal_Componets/footer/index.jsx
@@ -4,11 +4,18 @@ import './style.css';
 export const Footer = () => {
     return (
         <footer>
-            <div className='footer_facebook footer_logoredSocial' />
-            <div className='footer_x footer_logoredSocial' />
-            <div className='footer_contacto'>Contacto</div>
+            <div className='footer-contacto-container-redes-sociales'>
+                        <div className='footer-contacto-instagram footer-redSocial'/>
+                        <div className='footer-contacto-x footer-redSocial'/>
+                        <div className='footer-contacto-facebook footer-redSocial'/>
+            </div>
+
+            <div className='footer_contacto'>Contactanos</div>
+
             <div className='footer_comunidadMadridLogo' />
+
             <div className='footer_logoEscudo' />
+
         </footer>    
   )
 }
